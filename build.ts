73e404293c103790b3bda437424a80655f9e3c3c
as <as@asdeiMac.local>
2023-10-17 15:55:30 +0800

    设计路由

--- build.ts
+++ build.ts
@@ -10,7 +10,7 @@ let stack: string[] = [];//保存遍历文件夹的栈
 
 for (const target of fs.readdirSync(pages)){
   if(fs.statSync(join(pages, target)).isDirectory()) stack.unshift(target);//如果是文件夹就放进栈
-  /^\S+\.(tsx|ts)$/.test(target) && entrypoints.push(join(basePath, target));//匹配tsx｜ts为后缀的文件，并保存结果
+  /^\S+\.(tsx|ts)$/.test(target) && entrypoints.push(join(prefix, target));//匹配tsx｜ts为后缀的文件，并保存结果
 }
 while(!!stack.length){
   let currentFile = stack.shift()!;
@@ -18,7 +18,7 @@ while(!!stack.length){
     if(fs.statSync(join(pages, currentFile, target)).isDirectory()){//如果是文件夹就放进栈
       stack.unshift(join(currentFile, target));
     } else {
-      /^\S+\.(tsx|ts)$/.test(target) && entrypoints.push(join(basePath, currentFile, target));//匹配tsx｜ts为后缀的文件，并保存结果
+      /^\S+\.(tsx|ts)$/.test(target) && entrypoints.push(join(prefix, currentFile, target));//匹配tsx｜ts为后缀的文件，并保存结果
     }
   }
 }
