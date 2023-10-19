f623f9bcf96909db1844bc370c1436af38f4f07a
as <as@asdeiMac.local>
2023-10-19 10:58:24 +0800

    增加两个函数复刻

---
+++ server/tree.test.ts
@@ -0,0 +1,37 @@
+import { expect, test, describe } from "bun:test";
+import { findWildcard, countParams, longestCommonPrefix } from './tree'
+describe("tree.ts-测试查找路径参数", ()=>{
+    test("findWildcard", ()=>{
+        expect(findWildcard("/hu/:param")).toEqual({
+            param: "param",
+            index: 4,
+            valid: true
+        })
+        expect(findWildcard("/hu/:param/about")).toEqual({
+            param: "param",
+            index: 4,
+            valid: true
+        })
+        expect(findWildcard("/hu/*param/*about")).toEqual({
+            param: "param",
+            index: 4,
+            valid: true
+        })
+    })
+})
+
+describe("tree.ts-测试查找路径参数个数", ()=>{
+    test("countParams", ()=>{
+        expect(countParams("/hu/:param")).toBe(1)
+        expect(countParams("/hu/:param/*about")).toBe(2)
+        expect(countParams("/hu/*param/*about/:jiji")).toBe(3)
+    })
+})
+
+describe("tree.ts-最长公共前缀", ()=>{
+    test("longestCommonPrefix", ()=>{
+        expect(longestCommonPrefix("/hu/:param", "/hu/:param/*about")).toBe(10)
+        expect(longestCommonPrefix("/hu/:param", "/hu/:par")).toBe(8)
+        expect(longestCommonPrefix("/hu/:param", "/hu/*param/*about/:jiji")).toBe(4)
+    })
+})
--- server/tree.ts
+++ server/tree.ts
@@ -1,32 +1,75 @@
-class Node {
-    path: string = ""
-    indices:   string=""
-	wildChild: Boolean = false
-	nType:     number = 0
-	priority:  number = 0
-	children:  Node[] = []
-	handle:    (req: Request)=>Response = (req) => new Response()
 
-    incrementChildPrio(pos: number) {
+//最长公共前缀个数
+export function longestCommonPrefix(a: string, b: string): number {
+	let i = 0;
+	let max = Math.min(a.length, b.length);
+	while (i < max && a[i] == b[i]) {
+		i++;
+	}
+	return i;
+}
+
+
+export function findWildcard(path: string) {
+	for (let start = 0; start < path.length; start++) {
+		if (path[start] != ':' && path[start] != '*') continue;
+		let valid = true;
+		const paramStr = path.slice(start + 1);
+		for (let end = 0; end < paramStr.length; end++) {
+			switch (paramStr[end]) {
+				case '/':
+					return { param: path.slice(start + 1, start + 1 + end), index: start, valid };
+				case ':':
+				case '*':
+					valid = false;
+			}
+		}
+		return { param: path.slice(start + 1), index: start, valid };
+	}
+	return { param: "", index: -1, valid: false };
+}
+
+export function countParams(path: string): number {
+	let n = 0;
+	for (const c of path) {
+		switch (c) {
+			case ':':
+			case '*':
+				n++;
+		}
+	}
+	return n;
+}
+
+class Node {
+	path: string = ""
+	indices: string = ""
+	wildChild: Boolean = false
+	nType: number = 0
+	priority: number = 0
+	children: Node[] = []
+	handle: (req: Request) => Response = (req) => new Response()
+
+	incrementChildPrio(pos: number) {
 		let cs = this.children;
 		cs[pos].priority++;
 		let prio = cs[pos].priority;
 
-	// Adjust position (move to front)
+		// Adjust position (move to front)
 		let newPos = pos;
-		while(newPos > 0 && cs[newPos-1].priority < prio) {//子节点从小到大排序
+		while (newPos > 0 && cs[newPos - 1].priority < prio) {//子节点从小到大排序
 			// Swap node positions
-			[cs[newPos-1], cs[newPos]] = [cs[newPos], cs[newPos-1]];
+			[cs[newPos - 1], cs[newPos]] = [cs[newPos], cs[newPos - 1]];
 			newPos--;
 		}
 
-	// Build new index char string 索引
-	if (newPos != pos) {
-		this.indices = this.indices.slice(0, newPos) + // Unchanged prefix, might be empty
-			this.indices.slice(pos, pos+1) + // The index char we move
-			this.indices.slice(newPos, pos) + this.indices.slice(pos+1) // Rest without char at 'pos'
-	}
+		// Build new index char string 索引
+		if (newPos != pos) {
+			this.indices = this.indices.slice(0, newPos) + // Unchanged prefix, might be empty
+				this.indices.slice(pos, pos + 1) + // The index char we move
+				this.indices.slice(newPos, pos) + this.indices.slice(pos + 1) // Rest without char at 'pos'
+		}
 
-	return newPos
+		return newPos
 	}
 }
