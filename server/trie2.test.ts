56b799365c57bac79398347a97d3c6157461de14
as <as@asdeiMac.local>
2023-10-24 17:29:53 +0800

    优化路由代码结构

--- server/trie2.test.ts
+++ server/trie2.test.ts
@@ -1,15 +1,31 @@
 import { expect, test, describe } from "bun:test";
-import { Node, insertNode, searchNode } from './trie2';
+import { Node, insertNode, searchNode, Context } from './trie2';
 describe("测试前缀树(new)", ()=>{
     test("Insert Test", ()=>{
         const node: Node = {
             children: {},
-            part: "",
-            wildChild: false
+            part: "hello",
+            wildChild: false,
+            index: 0
         };
-        insertNode(node, "/hello/8080");
-        const targetNode = searchNode(node, "/hello/8080");
-        console.log(targetNode);
-        expect(targetNode?.wildChild).toBe(true);
+        const handle = (ctx: Context) => {
+            return new Response(ctx.req.url)
+        }
+        const handle2 = (ctx: Context) => {
+            return new Response(ctx.req.url)
+        }
+        insertNode(node, 'GET', "/hello/:x/:y", handle);
+        let target = searchNode(node, 'GET', "/hello/90/80");
+        expect(target[1]['x']).toEqual("90")
+        insertNode(node, 'POST', "/hello/:x/:y", handle2);
+        target = searchNode(node, 'POST', "/hello/90/80");
+        expect(target[1]['x']).toEqual("90")
+        expect(target[0]).toEqual(handle2)
+        insertNode(node, 'GET', "/hello/:x/hu", handle);
+        target = searchNode(node, "GET", "/hello/99/hu");
+        expect(target[1]['x']).toEqual("99")
+        insertNode(node, 'POST', "/hello/:x/*target", handle2);
+        target = searchNode(node, "POST", "/hello/80/hu/max");
+        expect(target[1]['target']).toEqual("hu/max");
     })
 })
--- server/trie2.ts
+++ server/trie2.ts
@@ -1,42 +1,79 @@
+import assert from "assert";
+
+type Context = {
+    params: Record<string, string>,
+    req: Request
+}
+
+type HTTPHandler = (context: Context)=>Response
+
 type Node = {
     children: Record<string, Node>,
     part: string,
     wildChild: Boolean,
-    fullPath?: string
+    fullPath?: string,
+    parent?: Node,
+    index: number,
+    handle?: Record<string, (context: Context)=>Response>
 }
 
-function insertNode(node: Node, path: string){
+function insertNode(node: Node, method: string, path: string, handle: (context: Context)=>Response ){
     let target = Object.assign({}, node);
     const parts = path.split('/').slice(1);
-    for (const part of parts) {
-        let child = node.children[part];
+    for(let start = 1; start < parts.length; start++) {
+        let child = target.children[parts[start]];
         if(child){
             target = child;
         } else {
-            target = target.children[part] = {
+            target = target.children[parts[start]] = {
                 children: {},
-                part,
-                wildChild: false
+                part: parts[start],
+                wildChild: parts[start][0] === "*"|| parts[start][0] === ":",//叶节点,
+                fullPath: parts.slice(0, start).join('/'),//用作节点的唯一ID
+                parent: target,
+                index: start
             }
         }
     }
     target.fullPath = path;
     target.wildChild = true;
+    if(!target.handle) target.handle = {};
+    target.handle![method] = handle;
 }
 
-function searchNode(node: Node, path: string){
+function searchNode(node: Node, method: string, path: string): [HTTPHandler, Record<string, string>]{
     const parts = path.split('/').slice(1);
-    let current = node;
+    assert(parts.length > 0);
+    let params: Record<string, string> = {};
     let index = 0;
-    while(index < parts.length){
-        let child = current.children[parts[index]];
-        if(!child){
-            return null;
+    let stack: Node[] = [node];
+    let current: Node;
+    do {
+        current = stack.pop()!;//取出栈顶
+        if(current.index == index) {
+            if(current.part === parts[index]){//精准匹配
+                index++;
+                for(const child of Object.keys(current.children)){
+                    stack.push(current.children[child]);
+                }
+            }
+            if(current.part[0] === ':'){//模糊匹配
+                for(const child of Object.keys(current.children)){
+                    stack.push(current.children[child]);
+                }
+                params[current.part.slice(1)] = parts[index];
+                index++;
+            }
+            if(current.part[0] === '*' && current.wildChild ){
+                params[current.part.slice(1)] = parts.slice(index).join('/')
+                return [current.handle![method], params]
+            }
+        } else {
+            index--;
         }
-        current = child;
-        index++;
-    }
-    return current;
+    } while(index < parts.length && stack.length > 0);
+    return [current.handle![method], params];
 }
 
 export { Node, insertNode, searchNode }
+export { Context, HTTPHandler }
