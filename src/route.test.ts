9ee2e09000aa334ee2e7d251b9f1893d09e138ac
as <as@asdeiMac.local>
2023-10-25 19:10:53 +0800

    测试修改

--- src/route.test.ts
+++ src/route.test.ts
@@ -6,9 +6,10 @@ describe("测试路由合并", ()=>{
     test("Compose Test", ()=>{
         const route0 = createRouter();
         const route1 = createRouter({ prefix: '/hello'});
-        const hand = (ctx: Context)=>new Response();
+        const hand = (ctx: Context)=>{ return new Response()};
+        const hand2 = (ctx: Context)=>{ return new Response("helo")};
         route0.GET("/huhu", hand);
-        route1.GET("/:bker", hand);
+        route1.GET("/:bker", hand2);
 
         compose(route0, route1);
         
@@ -16,6 +17,6 @@ describe("测试路由合并", ()=>{
         // console.log(route0.tree.children['hello'].children)
         expect(params['bker']).toBe("89");
         let [node2, params2] = searchNode(route0.tree, 'GET', '/hello/huhu');
-        expect(node2).toEqual(hand);
+        expect(node2).toEqual(hand2);
     })
 })
